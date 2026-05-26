98a6cf3f2fd12ba49146d1c21efd106fcf165fa9
brunonicvl-usp <brunonicladosky@usp.br>
2026-05-26 15:16:24 -0300

    Upload com a lógica do semaforo

--- Aula02/semaforo.ino
+++ Aula02/semaforo.ino
@@ -1,1 +1,15 @@
-aaaaa
+#define LED_BUILTIN 8
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  neopixelWrite(LED_BUILTIN, 0, 50, 0);  // verde
+  delay(3000);                      // 3 seconds
+  neopixelWrite(LED_BUILTIN, 50, 50, 0);   // amarelo
+  delay(1000);                      // 1 second
+  neopixelWrite(LED_BUILTIN, 50, 0, 0);   // vermelho
+  delay(4000);                      // 4 seconds
+}
